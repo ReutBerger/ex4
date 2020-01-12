1ef1da8c75b3a90dbad395fd60c46f38e11a995e
reut <reutbrgr@gmail.com>
2020-01-12 13:17:31 +0200

    declarete classes

---
+++ MyTestClientHandler.h
@@ -0,0 +1,18 @@
+//
+// Created by reut on 1/12/20.
+//
+
+#ifndef EX4_MYTESTCLIENTHANDLER_H
+#define EX4_MYTESTCLIENTHANDLER_H
+
+#include "ClientHandler.h"
+
+class MyTestClientHandler : public ClientHandler {
+private:
+   // Solver solver;
+   // CacheManger am;
+
+};
+
+
+#endif //EX4_MYTESTCLIENTHANDLER_H
