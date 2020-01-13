249586f6fabda28d9e32ac144ce3c3897b78be17
reut <reutbrgr@gmail.com>
2020-01-13 13:54:06 +0200

    main for testing

--- main.cpp
+++ main.cpp
@@ -15,11 +15,16 @@ namespace boot{
 
 #include "StringReverse.h"
 class boot::Main {
-    int main(int argc, char *argv[]) {
+public:
+    static int main(int port) {
         StringReverse *s = new StringReverse();
         cout << s->Solve("abcd") << endl;
         return 0;
     }
 };
 
+int main(int argc, char *argv[]) {
+//    boot::Main::main(atoi(argv[1]));
+    boot::Main::main(atoi("1234"));
+}
 #endif //EX4_BOOT_H
